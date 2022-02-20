6f30971e70bdb4268ce5ff08d379ac95a3510997
Vaibhav Sharma <vaibhavsharma.v@udaan.com>
2022-02-21 01:09:08 +0530

    allow speed from get (#383)
    * allow speed from get
    
    * updating the version to use effor param in libvips

--- options.go
+++ options.go
@@ -150,6 +150,7 @@ func BimgOptions(o ImageOptions) bimg.Options {
 		Rotate:         bimg.Angle(o.Rotate),
 		Interlace:      o.Interlace,
 		Palette:        o.Palette,
+		Speed:          o.Speed,
 	}
 
 	if len(o.Background) != 0 {
